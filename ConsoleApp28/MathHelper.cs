16d117140f588db18c0eeaa3566ade726709e87d
LikoKuridze <lkuridze@credo.ge>
2024-11-18 12:48:11 +0400

    Add project files.

---
+++ ConsoleApp28/MathHelper.cs
@@ -0,0 +1,31 @@
+﻿
+namespace ConsoleApp28
+{
+    public static class MathHelper
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(int a, int b)
+        {
+            // თუ b არის 0, ვისვრით Exception-ს
+            if (b == 0)
+            {
+                throw new Exception("ნულზე გაყოფა შეუძლებელია!");
+            }
+            return (double)a / b;
+        }
+    }
+}
---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp28;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number1 = 10;
+        int number2 = 5;
+
+        // შეკრება
+        int sum = MathHelper.Add(number1, number2);
+        Console.WriteLine($"შეკრება: {number1} + {number2} = {sum}");
+
+        // გამოკლება
+        int difference = MathHelper.Subtract(number1, number2);
+        Console.WriteLine($"გამოკლება: {number1} - {number2} = {difference}");
+
+        // გამრავლება
+        int product = MathHelper.Multiply(number1, number2);
+        Console.WriteLine($"გამრავლება: {number1} * {number2} = {product}");
+
+        // გაყოფა
+        try
+        {
+            double quotient = MathHelper.Divide(number1, number2);
+            Console.WriteLine($"გაყოფა: {number1} / {number2} = {quotient}");
+
+            Console.WriteLine("\nვცდილობთ ნულზე გაყოფას:");
+            double divideByZero = MathHelper.Divide(number1, 0);
+        }
+        catch (Exception ex)
+        {
+            // დაიჭერს და დაბეჭდავს Exception-ის მესიჯს
+            Console.WriteLine($"შეცდომა: {ex.Message}");
+        }
+
+    }
+}
